0615744bff807af23874cd8292c18ebe435c4c0c
nat <natavan.vali@gmail.com>
2024-12-13 14:35:09 +0400

    Initial Commit

---
+++ MovieApp2Tests/MovieApp2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp2Tests.swift
+//  MovieApp2Tests
+//
+//  Created by Natavan Valiyeva on 13.12.24.
+//
+
+import Testing
+@testable import MovieApp2
+
+struct MovieApp2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
